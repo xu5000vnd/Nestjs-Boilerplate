6f040b57af73a77ef800a9f12ecd2b3a92d8208b
sonHL <sonhl@cloudverse.ai>
2023-06-23 00:16:47 +0700

    feat: update filter global exception

--- src/common/filters/global-exception.filter.ts
+++ src/common/filters/global-exception.filter.ts
@@ -13,20 +13,23 @@ export class GlobalExceptionsFilter implements ExceptionFilter {
     const ctx = host.switchToHttp()
     const request = ctx.getRequest<Request>()
     const response = ctx.getResponse<Response>()
-
     const status =
       exception instanceof HttpException
         ? exception.getStatus()
         : HttpStatus.INTERNAL_SERVER_ERROR
-
-    response.status(status).json({
-      statusCode: status,
-      message:
-        exception instanceof HttpException
-          ? exception.getResponse()
-          : 'Internal Server Error',
-      timestamp: new Date().toISOString(),
-      path: request.url,
-    })
+    if (exception instanceof HttpException) {
+      const dataResponse = exception.getResponse()
+      response.status(status).json(dataResponse)
+    } else {
+      response.status(status).json({
+        statusCode: status,
+        message:
+          exception instanceof HttpException
+            ? exception.getResponse()
+            : 'Internal Server Error',
+        timestamp: new Date().toISOString(),
+        path: request.url,
+      })
+    }
   }
 }
